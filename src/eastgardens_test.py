e6fe1c013c277ee315f15114549733f8a79eebbf
Stuart Auld <stuart.auld@viostream.com>
2020-02-05 19:17:21 +1100

    a few additions

---
+++ src/eastgardens_test.py
@@ -0,0 +1,26 @@
+from eastgardens import eastgardens
+import variables
+
+
+def test_eastgardens_vanilla():
+    variables.HOST = 'test.com'
+    response = eastgardens(
+        {'Records': [{'cf': {'request': {'uri': '/thing'}}}]}, None)
+    assert response == {'headers': {'location': [
+        {'key': 'Location', 'value': '//test.com/thing'}]}, 'status': '302', 'statusDescription': 'Found'}
+
+
+def test_eastgardens_origin():
+    variables.REDIRECTS = {'/nah-man': 'https://somewhere.com/wut'}
+    variables.FALLTHROUGH = 'origin'
+    response = eastgardens(
+        {'Records': [{'cf': {'request': {'uri': '/thing', 'otherstuff': 'yeah'}}}]}, None)
+    assert response == {'uri': '/thing', 'otherstuff': 'yeah'}
+
+
+def test_eastgarders_redirects():
+    variables.REDIRECTS = {'/a-hit': 'https://great-site.com/kitten-videos'}
+    response = eastgardens(
+        {'Records': [{'cf': {'request': {'uri': '/a-hit', 'otherstuff': 'yeah'}}}]}, None)
+    assert response == {'headers': {'location': [
+        {'key': 'Location', 'value': 'https://great-site.com/kitten-videos'}]}, 'status': '302', 'statusDescription': 'Found'}
---
+++ src/variables.py
@@ -0,0 +1,3 @@
+FALLTHROUGH = ''
+HOST = ''
+REDIRECTS = {}
